79329c9e59ad0fcd76f3b062f7d54cd4f00006b6
Atif <amnoori@users.noreply.github.com>
2017-05-05 16:10:12 -0700

    Wire up AddToDo and add UUID

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
+var uuid = require('node-uuid');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -11,24 +12,35 @@ var TodoApp = React.createClass({
 
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         }, {
-          id: 2,
+          id: uuid(),
           text: 'Clean the yard'
         }, {
-          id: 3,
+          id: uuid(),
           text: 'Mow the lawn'
         }, {
-          id: 4,
+          id: uuid(),
           text: 'Get Groceries'
         }
       ]
     };
   },
+
   handleAddTodo: function (text) {
-    alert('new todo: ' + text);
+    this.setState({
+      todos: [
+        //use spread operator to add the array of existing todo items
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    });
   },
+
   handleSearch: function (showCompleted, searchText) {
     this.setState ({
       showCompleted: showCompleted,
---
+++ playground/spread.js
@@ -0,0 +1,16 @@
+// function add (a,b) {
+//   return a+b;
+// }
+// console.log(add(3,1));
+//
+// var toAdd = [9,5];
+//
+// console.log(add(...toAdd));
+
+var groupA = ['Jen', 'Cory'];
+var groupB = ['Ash'];
+var final = [3, groupA]
+var final2 = [3, ...groupA];
+
+console.log(final);
+console.log(final2);
