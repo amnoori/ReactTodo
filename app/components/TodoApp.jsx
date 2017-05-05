58a15dfa7a8dad2a69a4119b5f8bf2fb45e372e4
Atif <amnoori@users.noreply.github.com>
2017-05-05 16:34:48 -0700

    Toggle Todos

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -13,16 +13,20 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
+          text: 'Walk the dog',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Clean the yard'
+          text: 'Clean the yard',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Mow the lawn'
+          text: 'Mow the lawn',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Get Groceries'
+          text: 'Get Groceries',
+          completed: false
         }
       ]
     };
@@ -35,10 +39,20 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed =! todo.completed; //sets to the opposite state. e.g. from true to false.
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
   },
 
   handleSearch: function (showCompleted, searchText) {
@@ -52,7 +66,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch = {this.handleSearch}/>
-        <TodoList todos = {todos}/>
+        <TodoList todos = {todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo = {this.handleAddTodo}/>
       </div>
     )
